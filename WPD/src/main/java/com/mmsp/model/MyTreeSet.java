fee69d9738d899b2e28d5870494eab453fad4b16
RoyAl-Vitamin <roalvi95@gmail.com>
2016-08-29 13:10:20 +0500

    Добавлена функциональность
    Теперь есть возможность создавать модули и разделы в тематическом плане,
    и они будут отображаться в древе тематического плана
    
    Signed-off-by: RoyAl-Vitamin <roalvi95@gmail.com>

---
+++ WPD/src/main/java/com/mmsp/model/MyTreeSet.java
@@ -0,0 +1,58 @@
+package com.mmsp.model;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+/**
+ * свой TreeSet с компаратором и методами
+ * @author rav
+ */
+public class MyTreeSet extends TreeSet<Semester> implements Comparator<Semester> {
+
+	private static final long serialVersionUID = -8219796252512673673L;
+
+	/**
+	 * Возвращает семестр по его номеру или null, если семестр не найден
+	 * @param number номер семестра
+	 * @return @Semester or null
+	 */
+	public Semester getSemester(int number) {
+		for (Semester sem : this) {
+			if (sem.getNUMBER_OF_SEMESTER() == number) return sem;
+		}
+		return null;
+	}
+
+	/*
+	 * Пусть сразу с компаратором будет
+	 */
+	@Override
+	public int compare(Semester o1, Semester o2) {
+		return o1.getNUMBER_OF_SEMESTER() - o2.getNUMBER_OF_SEMESTER();
+	}
+
+	@Override
+	public String toString() {
+		String s = "";
+		// спустимся по дереву
+		System.err.println();
+		for (Semester sem : this) {
+			System.err.println(" + ");
+			System.err.println(" +> Семестр № " + sem.getNUMBER_OF_SEMESTER() + " количество недель в нём == " + sem.getQUANTITY_OF_WEEK());
+			for (Module mod : sem.getTreeModule()) {
+				System.err.println(" + ");
+				System.err.println(" ++> " + "модуль № " + mod.getNumber() + " и его название " + mod.getName());
+				for (Section sec : mod.getSetSection()) {
+					System.err.println(" + ");
+					System.err.println(" ++++>  + " + " секция № " + sec.getNumber() + " и её название " + sec.getName());
+					for (ThematicPlan theme : sec.getSetTheme()) {
+						System.err.println(" + ");
+						System.err.println(" ++++++> " + " тема № " + theme.getNumber() + " и её название " + theme.getTitle());
+					}
+				}
+			}
+		}
+		System.err.println();
+		return s;
+	}
+}
